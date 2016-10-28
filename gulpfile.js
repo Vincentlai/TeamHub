42ab509bd3cb30ac0d5e8022e48d47f6eaf0a95c
qla64 <qla64@sfu.ca>
2016-10-28 16:41:56 -0700

    front end tools setup

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+/**
+ * Gulp config file
+ * Created by Qiang Lai on 2016/10/28.
+ */
+
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    minifycss = require('gulp-minify-css'),
+    // jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    notify = require('gulp-notify'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+
+gulp.task('styles', function () {
+   return gulp.src('src/less/*.less')
+       .pipe(less())
+       .pipe(gulp.dest('public/css'))
+       .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// gulp.task('scripts', function () {
+//    return gulp.src('public/js/*.js')
+//        .pipe(jshint('.jshintrc'))
+//        .pipe(jshint.reporter('default'))
+//        .pipe(notify({message: 'scripts task complete'}));
+// });
+
+gulp.task('clean', function (cb) {
+   del(['public/css/*'], cb)
+});
+
+gulp.task('default', function () {
+    gulp.run('clean');
+    gulp.run('styles');
+    gulp.watch('./src/less/*.less', function(){
+        gulp.run('styles');
+    });
+    gulp.watch(['public/css/*']).on('change', livereload.changed);
+});
+
+
--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
 
     app.get('/', function(req, res)
     {
-        console.log("-> a user visited the page")
+        console.log("-> a user visited the page");
         console.log("** session_id: " + req.session.id);
         res.sendFile(__dirname + '/index.html');
     });
