fb2f08e500a46c7483f98e7754a79900634a3709
qla64 <qla64@sfu.ca>
2016-11-01 21:56:08 -0700

    added notification on login page

--- public/js/ui-controllers/user-controller.js
+++ public/js/ui-controllers/user-controller.js
@@ -6,7 +6,8 @@
     var module = angular.module('user-controller', []);
     module.service('UserService',[
         '$http',
-        function ($http) {
+        '$timeout',
+        function ($http, $timeout) {
             var me = this;
             me.data = {};
             me.login = function() {
@@ -14,14 +15,43 @@
                 $http.post('/users/login', me.data)
                     .then(function (r) {
                         if (r.data.status == 1){
-
+                            me.msg = r.data.msg;
+                            me.error = true;
+                            $timeout(function () {
+                                me.error = false;
+                            }, 4000);
                         }else{
-                            me.login_failed == true;
+                            me.msg = r.data.msg;
+                            me.error = true;
+                            $timeout(function () {
+                                me.error = false;
+                            }, 4000);
                         }
                     }, function (e) {
                         console.log(e.data);
                     });
             }
+            me.signup = function() {
+                console.log('signup');
+            //     $http.post('/users/login', me.data)
+            //         .then(function (r) {
+            //             if (r.data.status == 1){
+            //                 me.msg = r.data.msg;
+            //                 me.error = true;
+            //                 $timeout(function () {
+            //                     me.error = false;
+            //                 }, 4000);
+            //             }else{
+            //                 me.msg = r.data.msg;
+            //                 me.error = true;
+            //                 $timeout(function () {
+            //                     me.error = false;
+            //                 }, 4000);
+            //             }
+            //         }, function (e) {
+            //             console.log(e.data);
+            //         });
+             }
         }
     ]);
     module.controller('login', [
@@ -30,5 +60,34 @@
         function ($scope, UserService)
         {
             $scope.User = UserService;
-        }]);
+        }
+        ]
+    );
+    module.controller('signup', [
+            '$scope',
+            'UserService',
+            function ($scope, UserService)
+            {
+                $scope.User = UserService;
+            }
+        ]
+    );
+    var checkPassword = function () {
+        return {
+            require: "ngModel",
+            scope: {
+                otherModelVale: "=checkPassword"
+            },
+            link:
+                function(scope, element, attributes, ngModel) {
+                    ngModel.$validators.checkPassword = function (value) {
+                        return value == scope.otherModelVale;
+                    };
+                    scope.$watch("otherModelValue", function () {
+                      ngModel.$validate();
+                    });
+                }
+        }
+    };
+    module.directive('checkPassword', checkPassword);
 }());
