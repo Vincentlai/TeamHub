0d9d2b7ef514625ca2aa96212ea3e45aac428996
Tommy Wu <hwa75@sfu.ca>
2016-11-09 17:07:45 -0800

    added api descriptions

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,6 +2,23 @@ var express = require('express');
 var router = express.Router();
 var post = require('../controllers/post.js');
 
+/* PATH: host_url:8080/posts/post (POST)
+ *
+ * INPUT: 'team_id' : team post to
+ *        'text' : post content
+ * 
+ * OUTPUT:
+ *  JSON Object that contains
+ *  'code' : respond code
+ *  'msg' : respond message
+ *
+ *   1 -> Post success
+ *  -1 -> empty post
+ *  -2 -> Invalid team_id
+ *  -9 -> No session, login required
+ *  -10 -> Missing fields
+ * 
+ */
 router.post('/post', function (req, res) {
 
     console.log("-> post called");
