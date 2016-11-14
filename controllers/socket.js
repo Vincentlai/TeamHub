0dec724640664c7e6f99d2cf5391397cd23468e2
Tommy Wu <hwa75@sfu.ca>
2016-11-14 00:58:22 -0800

    implemented basic public chat room

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,10 +1,9 @@
 var models = require('../models/models.js');
 
 onlineUsersList = [];
-module.exports = function(io)
-{
+module.exports = function (io) {
 
-    io.on('connection', function(socket){
+    io.on('connection', function (socket) {
         /* on connect */
         console.log('-> a user connected through socket');
         console.log('** socket_id: ' + socket.id);
@@ -13,27 +12,27 @@ module.exports = function(io)
 
         // add new connected user to the onlineUser list
         onlineUsersList.push({
-                user_id : socket.handshake.session.user_id,
-                socket_id : socket.id
-            })
+            user_id: socket.handshake.session.user_id,
+            socket_id: socket.id
+        })
         console.log('** current online users: ' + onlineUsersList.length + "\n")
 
         /* on disconnect */
-        socket.on('disconnect', function(){
+        socket.on('disconnect', function () {
             console.log('-> user disconnected from socket');
             console.log('** socket_id: ' + this.id);
 
             // remove disconnected user from the onlineUser list
-            for(var i=0;i<onlineUsersList.length;i++){
-                if(onlineUsersList[i].socket_id == socket.id){
-                    onlineUsersList.splice(i,1);
+            for (var i = 0; i < onlineUsersList.length; i++) {
+                if (onlineUsersList[i].socket_id == socket.id) {
+                    onlineUsersList.splice(i, 1);
                 }
             }
             console.log('** current online users: ' + onlineUsersList.length + "\n")
         });
 
         /* on chat room mesasge */
-        socket.on('chat message', function(msg){
+        socket.on('chat message', function (msg) {
             console.log("-> public chat message received");
             console.log("** socket_id: " + this.id);
             console.log("** session_id: " + socket.handshake.session.id);
@@ -42,7 +41,17 @@ module.exports = function(io)
             io.emit('chat message', msg);
         });
 
-        
+        socket.on('team_msg', function (json) {
+            
+            console.log("echo " + json.msg + " " +json.team_id + " " + json.uuid);
+
+            io.emit('team_msg', json);
+        });
+
+
+
+
+
         /* on private message */
         /*
         socket.on('private message', function(obj) {
--- public/js/chat.js
+++ public/js/chat.js
@@ -4,18 +4,73 @@ app.config(function ($socketProvider) {
     $socketProvider.setConnectionUrl('http://localhost:8080');
 });
 
+
 app.controller('Ctrl', function Ctrl($scope, $socket) {
-    
-    $socket.on('echo', function (data) {
-        $scope.serverResponse = data;
-    });
-    
-    $scope.emitBasic = function emitBasic() {
-        console.log('echo event emited');
-        $socket.emit('echo', $scope.dataToSend);
-        $scope.dataToSend = '';
+
+    function guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+    }
+
+    var index = 0;
+    var role_arr = [];
+    var self_icon = "http://i.imgur.com/HYcn9xO.png";
+    var other_icon = "http://i.imgur.com/DY6gND0.png";
+    // generate a GUID
+    var uuid = guid();
+    // initialize msg list
+    $scope.msg_list = [];
+
+    $scope.getClass = function(index){
+        return role_arr[index].class;
     };
-    
+
+    $scope.getSrc = function(index){
+        return role_arr[index].src;
+    };
+
+    /* on receive team message */
+    $socket.on('team_msg', function (json) {
+
+        if (json.uuid != uuid) { // if not my message
+            // update ui
+            var msg = json.msg;
+            var time = new Date().getHours() + ":" + new Date().getMinutes();
+            //$scope.role_class = getClass;
+            //$scope.icon_scr = getSrc;
+            $scope.msg_list.push({ index, msg, time });
+            role_arr.push({class:"other", src:other_icon});
+            index++;
+        }
+    });
+
+    /* on send team message */
+    $scope.emitTeamMsg = function emitTeamMsg() {
+
+        // send json to server
+        var json = {};
+        var msg = $scope.dataToSend;
+        json.msg = msg;
+        json.team_id = "1234567890"; // tmp: to be changed
+        json.uuid = uuid;
+
+        $socket.emit('team_msg', json);
+
+        // update ui
+        $scope.dataToSend = '';
+        var time = new Date().getHours() + ":" + new Date().getMinutes();
+        //$scope.role_class = getClass;
+        //$scope.icon_scr = getSrc;
+        $scope.msg_list.push({ index, msg, time });
+        role_arr.push({class:"self", src:self_icon});
+        index++;
+    };
+
     $scope.emitACK = function emitACK() {
         $socket.emit('echo-ack', $scope.dataToSend, function (data) {
             //se nao tivesse sido feito $apply
